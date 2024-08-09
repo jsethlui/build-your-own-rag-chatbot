81c6070731e20fc3f457b152fb48d98703675194
Jeremy Louie <jeremysf74@yahoo.com>
2024-08-08 20:58:01 -0700

    /upload/ endpoint adds to vector database * Updated requirements to have specific versions * File upload endpoint adds to vector database * New functions added to utils

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,32 +1,53 @@
 
+import os
+import json
 import yaml
+import logging
+from dotenv import load_dotenv
+from .utils import fileExtensionToLoader, loadConfig, getTextChunks
+
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, UploadFile, File
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate, ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.schema.runnable import RunnablePassthrough
+
+from langchain_community.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+
 app = FastAPI()
+config = loadConfig()
+load_dotenv()
 
 @app.post("/upload/")
-async def uploadFile(file: UploadFile = File(...), chunk=False):
+async def upload(file: UploadFile = File(...), chunk=False):
+    '''
+    Upload a file, and add to vector database
+    '''
+
     # @todo: incorporate chunk reading of file
     try:
-        contents = file.file.read()
+        rawContent = file.file.read()                   # Byte string
+        decodedContent = rawContent.decode("utf-8")     # String object
         stream = open(file.filename, "wb")
     except Exception as error:
         raise HTTPException(status_code=404, detail=error)
     finally:
         file.file.close()
         stream.close()
-    return {"fileName": file.filename, "fileContent": contents}
+
+    # Load document and split into chunks
+    docs = getTextChunks(decodedContent)
+
+    # Create embedding function and load into Chroma
+    embeddingFunction = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.getenv("API_KEY"))
+    database = Chroma.from_documents(docs, embeddingFunction)
+    retriever = database.as_retriever(k=10)
+    return {"name": file.filename, "size": file.size, "content": decodedContent}
 
 @app.get("/")
 async def root():
-    try:
-        stream = open("../config.yaml", "r")
-        data = yaml.safe_load(stream)
-    except yaml.YAMLError as error:
-        raise HTTPException(status_code=404, detail=error)
-    except IOError:
-        raise HTTPException(status_code=404, detail="Cannot load config.yaml")
-    finally:
-        stream.close()
-
-    return {"message": data["debug"]}
+    return config
